c6413669fda0d6b32840265b6621281c6c5c5337
blazarow09 <blazarow09@gmail.com>
2019-05-05 14:53:06 +0300

    upload SIS with implemented session storage and cookies

---
+++ C# Web Development Basics/Web Server - Session Storage - Using Cookies/SIS/SIS.HTTP/Responses/Contracts/IHttpResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace SIS.HTTP.Responses.Contracts
+{
+    using SIS.HTTP.Cookies;
+    using SIS.HTTP.Headers;
+    using SIS.HTTP.Headers.Contracts;
+    using System.Net;
+
+    public interface IHttpResponse
+    {
+        HttpStatusCode StatusCode { get; set; }
+
+        IHttpHeaderCollection Headers { get; }
+
+        byte[] Content { get; set; }
+
+        void AddHeader(HttpHeader header);
+
+        void AddCookie(HttpCookie cookie);
+
+        byte[] GetBytes();
+    }
+}
---
+++ C# Web Development Basics/Web Server - Session Storage - Using Cookies/SIS/SIS.HTTP/Responses/HttpResponse.cs
@@ -0,0 +1,79 @@
+﻿namespace SIS.HTTP.Responses
+{
+    using SIS.HTTP.Common;
+    using SIS.HTTP.Cookies;
+    using SIS.HTTP.Cookies.Contracts;
+    using SIS.HTTP.Headers;
+    using SIS.HTTP.Headers.Contracts;
+    using SIS.HTTP.Responses.Contracts;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+
+    public class HttpResponse : IHttpResponse
+    {
+        public HttpResponse()
+        {
+        }
+
+        public HttpResponse(HttpStatusCode statusCode)
+        {
+            this.Headers = new HttpHeaderCollection();
+            this.Cookies = new HttpCookieCollection();
+            this.Content = new byte[0];
+            this.StatusCode = statusCode;
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public IHttpHeaderCollection Headers { get; }
+
+        public IHttpCookieCollection Cookies { get; }
+
+        public byte[] Content { get; set; }
+
+        public void AddHeader(HttpHeader header)
+        {
+            this.Headers.Add(header);
+        }
+
+        public void AddCookie(HttpCookie cookie)
+        {
+            CoreValidator.ThrowIfNull(cookie, nameof(cookie));
+
+            this.Cookies.Add(cookie);
+        }
+
+        public byte[] GetBytes()
+        {
+            return Encoding.UTF8
+                .GetBytes(this.ToString())
+                .Concat(this.Content)
+                .ToArray();
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result
+                .Append($"{GlobalConstants.HttpOneProtocolFragment} {(int)this.StatusCode} {this.StatusCode}")
+                .Append(GlobalConstants.HttpNewLine)
+                .Append(this.Headers)
+                .Append(GlobalConstants.HttpNewLine);
+
+            if (this.Cookies.HasCookies())
+            {
+                foreach (var httpCookie in this.Cookies)
+                {
+                    result.Append($"Set-Cookie: {httpCookie}")
+                        .Append(GlobalConstants.HttpNewLine);
+                }
+            }
+
+            result.Append(GlobalConstants.HttpNewLine);
+
+            return result.ToString();
+        }
+    }
+}
